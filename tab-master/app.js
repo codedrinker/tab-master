05814bfa3c85d7da2d24b464e71d6145bed5ffc8
codedrinker <gwangchunlei@gmail.com>
2017-08-14 07:47:36 +0800

    add to block list functionality

--- tab-master/app.js
+++ tab-master/app.js
@@ -1,8 +1,24 @@
-chrome.storage.local.get('toggle', function(data) {
-	 if(data.toggle == true){
-	 	var links = document.getElementsByTagName('a');
-		for (var i = 0, l = links.length; i < l; i++) {
-		  links[i].target = '_blank';
-		}
-	 }
+chrome.storage.sync.get({
+    toggle: true,
+    block_list: [],
+}, function (items) {
+    var toggle = items.toggle;
+    var block_list = items.block_list;
+    if (!toggle) return;
+    if (isBlocked(block_list)) return;
+    var links = document.getElementsByTagName('a');
+    for (var i = 0, l = links.length; i < l; i++) {
+        links[i].target = '_blank';
+    }
 });
+function isBlocked(block_list) {
+    if (!block_list || block_list.length == 0) return false;
+    var domain = document.domain;
+    var block = false;
+    for (var i = 0; i < block_list.length; i++) {
+        if (domain.indexOf(block_list[i]) != -1) {
+            block = true;
+        }
+    }
+    return block;
+}
--- tab-master/background.js
+++ tab-master/background.js
@@ -1,50 +1,95 @@
-function displayEnable () {
-	chrome.contextMenus.removeAll();
-	chrome.contextMenus.create({
-	      title: "Enable",
-	      contexts: ["browser_action"],
-	      onclick: function() {
-	        enable();
-	        displayDisable();
-	        refresh();
-	      }
-	});
+function displayEnable() {
+    chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+        title: "Enable",
+        contexts: ["browser_action"],
+        onclick: function () {
+            enable();
+            displayDisable();
+            refresh();
+        }
+    });
+    chrome.contextMenus.create({
+        "title": "Add to Block List",
+        "contexts": ["page"],
+        "onclick": function (info) {
+            add2blocklist(info.pageUrl);
+        }
+    });
 }
 
 function displayDisable() {
-	chrome.contextMenus.removeAll();
-	chrome.contextMenus.create({
-	      title: "Disable",
-	      contexts: ["browser_action"],
-	      onclick: function() {
-	        disable();
-	        displayEnable();
-	        refresh();
-	      }
-	});
+    chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+        title: "Disable",
+        contexts: ["browser_action"],
+        onclick: function () {
+            disable();
+            displayEnable();
+            refresh();
+        }
+    });
+    chrome.contextMenus.create({
+        "title": "Add to Block List",
+        "contexts": ["page"],
+        "onclick": function (info) {
+            add2blocklist(info.pageUrl);
+        }
+    });
 }
 
 function enable() {
-	 chrome.storage.local.set({'toggle': true}, function() {
-     });
-     chrome.browserAction.setIcon({
-	  path : "icon.png"
-	 });
+    chrome.storage.sync.set({'toggle': true}, function () {
+    });
+    chrome.browserAction.setIcon({
+        path: "icon.png"
+    });
 }
 function disable() {
-	 chrome.storage.local.set({'toggle': false}, function() {
-     });
-     chrome.browserAction.setIcon({
-	  path : "close.png"
-	 });
+    chrome.storage.sync.set({'toggle': false}, function () {
+    });
+    chrome.browserAction.setIcon({
+        path: "close.png"
+    });
+}
+
+function checkToggle() {
+    chrome.storage.sync.get({
+        toggle: true
+    }, function (items) {
+        var toggle = items.toggle;
+        if (toggle) {
+            displayDisable();
+            enable();
+        } else {
+            displayEnable();
+            disable();
+        }
+    });
 }
 
 function refresh() {
-	chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.getSelected(null, function (tab) {
         tabId = tab.id;
         chrome.tabs.reload(tabId);
     });
 }
 
-enable();
-displayDisable();
+function getHostname(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l.hostname;
+}
+
+function add2blocklist(url) {
+    var hostname = getHostname(url);
+    chrome.storage.sync.get({block_list: []}, function (result) {
+        var block_list = result.block_list;
+        if (block_list.indexOf(hostname) != -1) return;
+        block_list.push(hostname);
+        chrome.storage.sync.set({block_list: block_list}, function () {
+
+        });
+    });
+}
+checkToggle();
